0c0d4bc4364797747ee7167f3606d39cdc5491e8
Equinaut19 <33580646+Equinaut19@users.noreply.github.com>
2021-04-14 23:48:13 +0100

    Improvements to interface
     - Grid spacing now changes depending on window size
     - Diagonal lines aren't allowed anymore
     - Line won't be added to list if the same line is already in the list

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,96 +1,114 @@
 const socket = io("http://localhost:3000");
-const SQUARE_SIZE = 75;
 
-let GRID_WIDTH = 6;
-let GRID_HEIGHT = 6;
-let board;
-let boardCanvas;
-let mouseX = mouseY = 0;
-let mouseDown = false;
-let dragStartPosition = [0, 0];
+let GRID_WIDTH = 5; //Width of the grid in dots
+let GRID_HEIGHT = 5; //Height of the grid in dots
+let SQUARE_SIZE = 100; //Distance between dots in pixels
+let TOTAL_PLAYERS = 1; //Total number of players in the current game
+let CURRENT_TURN = 0; //Player number of whoevers turn it is
+let PLAYER_NAMES = []; //Name of every player in the game
 
-let lines = [];
+let boardCanvas; //DOM element of HTML canvas
+let mouseX = mouseY = 0; //Mouse X and Y position relative to canvas updated whenever mouse is moved
+let mouseDown = false; //If the mouse is pressed or not
+let dragStartPosition; //Start location of mouse drag
+let selected = false; //If a dot is selected
+let selectedPosition; //Position of selected dot
 
-class Line {
+let lines = []; //Array of line objects
+
+class Line { //Class for line object
   constructor(startPosition, endPosition) {
-    this.startPosition = [SQUARE_SIZE*(startPosition[0]+0.5),SQUARE_SIZE*(startPosition[1]+0.5)];
-    this.endPosition = [SQUARE_SIZE*(endPosition[0]+0.5),SQUARE_SIZE*(endPosition[1]+0.5)];;
+    this.startPosition = [startPosition[0],startPosition[1]];
+    this.endPosition = [endPosition[0],endPosition[1]];
+    if (this.startPosition[0]+this.startPosition[1]>this.endPosition[0]+this.endPosition[1]) [this.startPosition, this.endPosition] = [this.endPosition, this.startPosition];
   }
-  draw() {
+  draw() { //Draws the line on the canvas
     let canvasContext = boardCanvas.getContext("2d");
-    canvasContext.moveTo(this.startPosition[0], this.startPosition[1]);
-    canvasContext.lineTo(this.endPosition[0], this.endPosition[1]);
+    canvasContext.lineWidth = 8;
+    canvasContext.beginPath();
+    canvasContext.moveTo((this.startPosition[0]+0.5)*SQUARE_SIZE, (this.startPosition[1]+0.5)*SQUARE_SIZE);
+    canvasContext.lineTo((this.endPosition[0]+0.5)*SQUARE_SIZE, (this.endPosition[1]+0.5)*SQUARE_SIZE);
     canvasContext.stroke();
+  }
+  equals(otherLine) { //Checks if a line is identical to the passed in line object
+    return this.startPosition.toString()==otherLine.startPosition.toString() && this.endPosition.toString()==otherLine.endPosition.toString();
   }
 }
 
+function createLine(startPosition, endPosition) { //Creates a new line with the given coordinates
+  let newLine = new Line(startPosition, endPosition); //New line object
+  let doesntExist = true;
+  for (line of lines) {
+    if (newLine.equals(line)) doesntExist = false; //Finds if the new line already exists
+  }
+  if (doesntExist) { //If line is new then add to list
+    lines.push(newLine);
+  }
+  delete newLine; //Deletes new line object
+  return doesntExist; //Returns boolean, if the new line was created or not
+}
 
 
-function startGame(boardWidth, boardHeight) {
+function startGame(boardWidth, boardHeight) { //Function called whenever a game begins
   GRID_WIDTH = boardWidth;
   GRID_HEIGHT = boardHeight;
-  board = [];
-  for (let y=0; y<boardHeight; y++) {
-    board[y] = [];
-    for (let x=0; x<boardWidth; x++) {
-      board[y][x] = null;
-    }
-  }
+
   boardCanvas = document.getElementById("board");
-  boardCanvas.width = (GRID_WIDTH + 1) * SQUARE_SIZE;
-  boardCanvas.height = (GRID_HEIGHT + 1) * SQUARE_SIZE;
+  boardCanvas.width = GRID_WIDTH * SQUARE_SIZE;
+  boardCanvas.height = GRID_HEIGHT * SQUARE_SIZE;
 }
 
 function drawGrid() {
-  if (boardCanvas == null) return;
+  if (boardCanvas == null) return; //If there is no canvas element, then return
 
-  boardCanvas.width = (GRID_WIDTH + 1) * SQUARE_SIZE;
-  boardCanvas.height = (GRID_HEIGHT + 1) * SQUARE_SIZE;
+  SQUARE_SIZE = Math.min((window.innerWidth - document.getElementById("board").getBoundingClientRect().x) / (GRID_WIDTH + 1), (window.innerHeight - document.getElementById("board").getBoundingClientRect().y) / (GRID_HEIGHT + 1));
+  //Set square size based on viewport size
+  boardCanvas.width = GRID_WIDTH * SQUARE_SIZE;
+  boardCanvas.height = GRID_HEIGHT * SQUARE_SIZE; //Change size of canvas
 
   let canvasContext = boardCanvas.getContext("2d");
-  for (let x = 0; x < GRID_WIDTH + 1; x++) {
-    for (let y = 0; y < GRID_HEIGHT + 1; y++) {
+  for (let x = 0; x < GRID_WIDTH; x++) {
+    for (let y = 0; y < GRID_HEIGHT; y++) {
       canvasContext.fillStyle = "black";
       canvasContext.beginPath();
       canvasContext.ellipse((x + 0.5) * SQUARE_SIZE, (y + 0.5) * SQUARE_SIZE, 8, 8, 0, 0, 2 * Math.PI);
-      canvasContext.fill();
+      canvasContext.fill(); //Draws dots for each dot on grid
     }
   }
 
   //Draw lines
-  for (let line of lines) line.draw();
-
-  //Draw cursor
-  canvasContext.beginPath();
-  canvasContext.ellipse(SQUARE_SIZE*(Math.floor(mouseX/SQUARE_SIZE)+0.5), SQUARE_SIZE*(Math.floor(mouseY/SQUARE_SIZE)+0.5), 15, 15, 0, 0, 2 * Math.PI);
-  canvasContext.stroke();
+  for (let line of lines) line.draw(); //Draws every line object
 
   if (mouseDown) {
-    let dragEndPosition = [Math.floor(mouseX/SQUARE_SIZE), Math.floor(mouseY/SQUARE_SIZE)];
-    if ((Math.abs(dragStartPosition[0] - dragEndPosition[0]) <= 1 && Math.abs(dragStartPosition[1] - dragEndPosition[1]) <= 1)) {
+    let dragEndPosition = [Math.floor(mouseX/SQUARE_SIZE), Math.floor(mouseY/SQUARE_SIZE)]; //End position of mouse drag
+    if ((Math.abs(dragStartPosition[0] - dragEndPosition[0]) + Math.abs(dragStartPosition[1] - dragEndPosition[1]) <= 1)) {
+      canvasContext.lineWidth = 8;
+      canvasContext.beginPath();
       canvasContext.moveTo((dragStartPosition[0]+0.5)*SQUARE_SIZE, (dragStartPosition[1]+0.5)*SQUARE_SIZE);
       canvasContext.lineTo((dragEndPosition[0]+0.5)*SQUARE_SIZE, (dragEndPosition[1]+0.5)*SQUARE_SIZE);
-      canvasContext.stroke();
+      canvasContext.stroke(); //Draws temporary line, to show where new line will be placed when mouse is released
     }
   }
 }
 
-startGame(10,10);
+startGame(GRID_WIDTH, GRID_HEIGHT); //Start game
 
 document.getElementById("board").addEventListener("mousemove", (mouseEvent) => {
   [mouseX, mouseY] = [mouseEvent.offsetX, mouseEvent.offsetY]
-});
+}); //Updates mouse position when mouse is moved
 
+//Mouse drag events
 document.getElementById("board").addEventListener("mousedown", (mouseEvent) => {
   mouseDown = true;
   dragStartPosition = [Math.floor(mouseX/SQUARE_SIZE), Math.floor(mouseY/SQUARE_SIZE)];
-});
+}); //Start of mouse drag
 
 document.getElementById("board").addEventListener("mouseup", (mouseEvent) => {
   mouseDown = false;
-  if ((Math.abs(dragStartPosition[0] - Math.floor(mouseX/SQUARE_SIZE)) <= 1 && Math.abs(dragStartPosition[1] - Math.floor(mouseY/SQUARE_SIZE)) <= 1)) {
-    lines.push(new Line(dragStartPosition, [Math.floor(mouseX/SQUARE_SIZE), Math.floor(mouseY/SQUARE_SIZE)]));
+  if ((Math.abs(dragStartPosition[0] - Math.floor(mouseX/SQUARE_SIZE)) +  Math.abs(dragStartPosition[1] - Math.floor(mouseY/SQUARE_SIZE)) == 1)) {
+    createLine(dragStartPosition, [Math.floor(mouseX/SQUARE_SIZE), Math.floor(mouseY/SQUARE_SIZE)]);
   }
-});
+}); //End of mouse drag, new line will be created if valid
 
-setInterval(drawGrid, 20);
+
+setInterval(drawGrid, 5); //Calls the draw function every 5 ms
